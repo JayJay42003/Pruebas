4c2c6ff4755952e6e57988b59dc6c248b8f51084
Joel Jimenez <joel.jimenez@altia.es>
2025-04-01 11:44:04 +0200

    Segundo commit-nuevo filewriter

--- New Maven/src/main/java/org/example/Main.java
+++ New Maven/src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        try {
+            FileWriter fw=new FileWriter("nuevo.txt");
+            
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
