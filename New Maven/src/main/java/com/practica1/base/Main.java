a08d9a8e84a596de99f13817c30dec85e4077d37
Joel Jimenez <joel.jimenez@altia.es>
2025-04-01 12:35:19 +0200

    Cambiado el nombre del paquete a com.practica1.base y quitados los puntos del "Hola mundo"

--- New Maven/src/main/java/com/practica1/base/Main.java
+++ New Maven/src/main/java/com/practica1/base/Main.java
@@ -1,14 +1,12 @@
-package org.example;
+package com.practica1.base;
 
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo....");
-
-
-
+        System.out.println("Hola Mundo");
+        
         try {
             FileWriter fw=new FileWriter("nuevo.txt");
             
