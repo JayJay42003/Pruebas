0ea8ef136714ed3c3086e718b6dc9139eb34f613
Joel Jimenez <joel.jimenez@altia.es>
2025-04-02 16:17:50 +0200

    Main limpiado de informacion sobrante

--- New Maven/src/main/java/com/practica1/base/Main.java
+++ New Maven/src/main/java/com/practica1/base/Main.java
@@ -28,12 +28,6 @@ public class Main {
             v.showInfo();
         };*/
 
-        //En el main, usar los métodos para añadir vehiculos de distinto tipo.
-        //
-        //Mostrar la informacion de los vehiculos utilizando los metodos implementados en el punto anterior.
-        //
-        //Usar TreeSet para ordenar por marca y volver a mostrar todos los vehiculos ordenados.
-
         Concesionario concesionario1=new Concesionario();
         concesionario1.addVehicle(new Car("123456XYZ","Mercedes","CLA Coupé",LocalDate.of(2004,6,23), FuelType.HYBRID.name(), 4));
         concesionario1.addVehicle(new Motorcycle("22361BVC","KTM","125 Duke",LocalDate.of(2012,10,3),FuelType.GASOLINE.name(),125));
@@ -41,18 +35,6 @@ public class Main {
         concesionario1.addVehicle(new Motorcycle("22364BVC", "Suzuki", "GSX-R1000", LocalDate.of(2019, 8, 18), FuelType.GASOLINE.name(), 999));
         concesionario1.addVehicle(new Car("1617181920PQR", "Volkswagen", "Golf", LocalDate.of(2018, 9, 9), FuelType.ELECTRIC.name(), 5));
 
-        /*
-        System.out.println("Encontrar vehiculo por matricula:");
-        concesionario1.findVehicleByLicensePlate("123456XYZ").showInfo();
-        System.out.println("Encontrar vehiculos por tipo:");
-        for (Vehicle v:concesionario1.filterByType(Car.class)){
-            v.showInfo();
-        }
-        System.out.println("Enseñar todas las matriculas:");
-        for (String str:concesionario1.getAllLicensePlates()){
-            System.out.println(str);
-        }
-        */
 
 
         TreeSet<Vehicle> vehicleTreeSet=new TreeSet<>();
