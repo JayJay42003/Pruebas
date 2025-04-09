2acc3c2c52948c90c9711e9c83ed3dfe80b374a5
Joel Jimenez <joel.jimenez@altia.es>
2025-04-09 11:16:11 +0200

    FuelType cambiados en el main y nombres de las clases adaptados

--- New Maven/src/main/java/com/practica1/base/Main.java
+++ New Maven/src/main/java/com/practica1/base/Main.java
@@ -1,10 +1,10 @@
 package com.practica1.base;
 
 import com.practica1.model.*;
-import com.practica1.model.dao.CarDAO;
-import com.practica1.model.dao.ConcessionaireDAO;
-import com.practica1.model.dao.MotorcycleDAO;
-import com.practica1.model.dao.VehicleDAO;
+import com.practica1.model.dao.CarDao;
+import com.practica1.model.dao.ConcessionaireDao;
+import com.practica1.model.dao.MotorcycleDao;
+import com.practica1.model.dao.VehicleDao;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -18,22 +18,22 @@ public class Main {
             dbC.initializeConnection();
             //Primera inyección de vehiculos.
             //dbC.insertInfo();
-            ConcessionaireDAO concessionaireDAO = new ConcessionaireDAO(dbC);
-            VehicleDAO vehicleDAO = new VehicleDAO(dbC);
-            MotorcycleDAO motorcycleDAO = new MotorcycleDAO(dbC);
-            CarDAO carDAO = new CarDAO(dbC);
+            ConcessionaireDao concessionaireDAO = new ConcessionaireDao(dbC);
+            VehicleDao vehicleDAO = new VehicleDao(dbC);
+            MotorcycleDao motorcycleDAO = new MotorcycleDao(dbC);
+            CarDao carDAO = new CarDao(dbC);
 
             //Crear concesionarios
             concessionaireDAO.createObject("conce1", 1);
             concessionaireDAO.createObject("conce2", 15);
 
             //Crear vehiculos
-            motorcycleDAO.createObject(new Motorcycle(1, 1, "5704GPO", "Yamaha", "MT-07", LocalDate.of(2019, 3, 20), "Gasoline", 689));
-            motorcycleDAO.createObject(new Motorcycle(2, 2, "5704GPN", "Honda", "CBR500R", LocalDate.of(2021, 7, 12), "Gasoline", 471));
-            motorcycleDAO.createObject(new Motorcycle(3, 1, "3333CCC", "Kawasaki", "Ninja 400", LocalDate.of(2020, 5, 30), "Gasoline", 399));
-            carDAO.createObject(new Car(1, 1, "5704GPN", "Toyota", "Corolla", LocalDate.of(2020, 1, 15), "Gasoline", 4));
-            carDAO.createObject(new Car(2, 2, "5704GPO", "Tesla", "Model 3", LocalDate.of(2022, 6, 10), "Electric", 4));
-            carDAO.createObject(new Car(3, 2, "9101GHI", "Volkswagen", "Golf", LocalDate.of(2018, 9, 5), "Diesel", 5));
+            motorcycleDAO.createObject(new Motorcycle(1, 1, "5704GPO", "Yamaha", "MT-07", LocalDate.of(2019, 3, 20), FuelType.GASOLINE.name(), 689));
+            motorcycleDAO.createObject(new Motorcycle(2, 2, "5704GPN", "Honda", "CBR500R", LocalDate.of(2021, 7, 12), FuelType.DIESEL.name(), 471));
+            motorcycleDAO.createObject(new Motorcycle(3, 1, "3333CCC", "Kawasaki", "Ninja 400", LocalDate.of(2020, 5, 30), FuelType.GASOLINE.name(), 399));
+            carDAO.createObject(new Car(1, 1, "5704GPN", "Toyota", "Corolla", LocalDate.of(2020, 1, 15), FuelType.GASOLINE.name(), 4));
+            carDAO.createObject(new Car(2, 2, "5704GPO", "Tesla", "Model 3", LocalDate.of(2022, 6, 10), FuelType.ELECTRIC.name(), 4));
+            carDAO.createObject(new Car(3, 2, "9101GHI", "Volkswagen", "Golf", LocalDate.of(2018, 9, 5), FuelType.DIESEL.name(), 5));
 
             //Leer coche por matricula y su concesionario
             Car car = carDAO.readObjectbyLicense("5704GPN");
